72cc403e2f2034469c7f02f1d7649a64500fe6b4
Mohit Kewlani <mmk9369@gr012.hpc.nyu.edu>
2022-05-14 19:13:13 -0400

    update

--- Extention1/extention1_LFM.py
+++ Extention1/extention1_LFM.py
@@ -6,6 +6,7 @@ from lightfm.evaluation import precision_at_k
 from scipy.sparse import csr_matrix
 from time import time
 from lightfm.cross_validation import random_train_test_split
+from lightfm.datasets import fetch_movielens
 
 def hyperParamTune(train, val, params, m_iter):
     
@@ -26,38 +27,32 @@ def hyperParamTune(train, val, params, m_iter):
 
 def main():
     
-    train_df = pd.read_csv('/scratch/mmk9369/movielens_small_train.csv')
-    test_df = pd.read_csv('/scratch/mmk9369/movielens_small_test.csv')
-    val_df = pd.read_csv('/scratch/mmk9369/movielens_small_val.csv')
+    movielens = fetch_movielens()
+    totalTrain, test = movielens['train'], movielens['test']
     
-    df = pd.concat([train_df, val_df, test_df])
-    dfNew = pd.pivot_table(df, index='userId', columns='movieId', values='rating')
-
-    df_matrix = csr_matrix(dfNew.values)
-    trainMatrix, valTestMatrix = random_train_test_split(df_matrix, test_percentage=0.8)
-    valMatrix, testMatrix = random_train_test_split(valTestMatrix, test_percentage=0.5)
+    train, val = random_train_test_split(totalTrain, test_percentage=0.7)
 
     params = {"rank": [100,120,140,160,180,200], "regParam": [0.01, 0.1, 1, 10]}
     
     st = time()
-    metrics = hyperParamTune(trainMatrix, valMatrix, params, m_iter = 4)
+    metrics = hyperParamTune(train, val, params, m_iter = 4)
     end = round(time()-st, 3)
     print("Hyperparameter tuning took {} seconds".format(end))
-
 
     maxMetric = 0
     for rank in metrics.keys():
         for regParam in metrics[rank]:
             if metrics[rank][regParam] > maxMetric:
+                maxMetric = metrics[rank][regParam]
                 maxRank = rank
                 maxRegParam = regParam         
-    bestRegParam, bestRank = float(str.split(maxRegParam, ' ')[0]), int(str.split(maxRank, ' ')[0])
+    bestRegParam, bestRank = float(str.split(maxRegParam, ' ')[2]), int(str.split(maxRank, ' ')[1])
     print("Best rank: {}, best reg: {}".format(bestRank, bestRegParam))
 
     st = time()
     model = LightFM(random_state = 123, learning_rate = bestRegParam, no_components = bestRank)
-    model = model.fit(train_df, epochs = 5)
-    metric =  precision_at_k(model, test_df).mean()
+    model = model.fit(train, epochs = 5)
+    metric =  precision_at_k(model, test).mean()
     end = round(time()-st, 3)
     
     print("Evaluation on test data: {}".format(metric))
@@ -65,4 +60,5 @@ def main():
     
     return pd.DataFrame(metrics)
 
-main()
+df = main()
+df.to_csv('LFM.csv')
